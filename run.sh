51d5db65e1bc258c6d32b6b89ec1a1c1a8ce924a
ramkashyap-s <2968129+ramkashyap-s@users.noreply.github.com>
2018-10-30 08:42:09 -0600

    project_setup

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 #
 # python3 ./src/h1b_counting.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
 
-python3 ./src/h1b_counting.py  -i ./input/H1B_FY_2016.csv  -o1 ./output/top_10_occupations.txt -o2 ./output/top_10_states.txt
+python3 ./src/h1b_counting.py  -i ./input/h1b_input.csv  -o1 ./output/top_10_occupations.txt -o2 ./output/top_10_states.txt
